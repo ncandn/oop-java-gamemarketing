c1894bbf3763534162db08c533820750816fab95
Vedat Özcan <vedat.ozcan@ceng.deu.edu.tr>
2020-04-06 23:54:52 +0300

    update

--- DigitalGameMarketing/src/Test.java
+++ DigitalGameMarketing/src/Test.java
@@ -1,23 +1,6 @@
 
 public class Test {
 	public static void main(String[] args) {
-		Game g1 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g2 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g3 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g4 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g5 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g6 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g7 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g8 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g9 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		Game g10 = new Game(null, null, null, null, 0, 0, 0, 0, null, null);
-		
-		
-		User u1 = new User(null, null, null, null, null, null, null, null, null, null, null);
-		User u2 = new User(null, null, null, null, null, null, null, null, null, null, null);
-		User u3 = new User(null, null, null, null, null, null, null, null, null, null, null);
-		User u4 = new User(null, null, null, null, null, null, null, null, null, null, null);
-		User u5 = new User(null, null, null, null, null, null, null, null, null, null, null);
-
+	 // new Interface();
 	}
 }
