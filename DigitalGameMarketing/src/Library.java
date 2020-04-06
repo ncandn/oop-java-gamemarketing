0f01e66893ad68b8537e5fe25cc08533f9a2f3b7
Vedat Özcan <vedat.ozcan@ceng.deu.edu.tr>
2020-04-07 00:08:04 +0300

    I will update the library class as soon as

---
+++ DigitalGameMarketing/src/Library.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+	private List<Game> games;
+
+	public Library() {
+		this.games = new ArrayList<Game>();
+	}
+
+	public List<Game> getGames() {
+		return games;
+	}
+
+	public void setGames(List<Game> games) {
+		this.games = games;
+	}
+	
+	
+
+	
+	
+	public String displayLibrary() {
+		return null;
+	}
+
+	public String displayLibrary(String genre) {
+		return null;
+	}
+
+	public void addGame() {
+
+	}
+
+	public void removeGame() {
+
+	}
+
+}
